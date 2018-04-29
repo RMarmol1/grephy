798b4894cf3ece67431917dc3c416f1ed8beda40
rmarmol1 <rafael.marmol1@marist.edu>
2018-04-29 17:37:51 -0400

    More with arguments and file reading
    4/29/18
    Added code to handle arguments and take in any file to read from

--- Driver_grephy.java
+++ Driver_grephy.java
@@ -8,58 +8,66 @@
  *
  * Class: Driver_grephy
  */
- 
+ import java.io.*;
  import java.util.Scanner;
  import java.util.regex.Matcher;
  import java.util.regex.Pattern;
  
  class Driver_grephy{
    
-   public static void main(String[] args){
+   public static void main(String[] args) throws FileNotFoundException {
      
-      Scanner input = new Scanner(System.in);
+      System.out.println(args[0]);
+      System.out.println(args[1]);
+      
+      String regex = args[0];
+      String fileName = args[1];
+      
+      File fileIn = new File (fileName);
+     
+     try{
+      Scanner input = new Scanner(fileIn);
       
       //learn alphabet
-     // String alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+     // String alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
        //int[] count = new int[52];
-       char[] alphabet = new char[50];
-       boolean dup = false;
-       int count = 0;
-       String file = "";
+      char[] alphabet = new char[50];
+      boolean dup = false;
+      int count = 0;
+      String file = "";
      
-       while(input.hasNext()){
-         file += input.next();
-       }
+      while(input.hasNext()){
+       file += input.next();
+     }
      
-       char[] fileArray = file.toCharArray();
+      char[] fileArray = file.toCharArray();
        
-       for(char c : fileArray){
-         //System.out.println(c);
-         dup = false;
-         for(int i = 0; i < alphabet.length; i++){
-           if(c == alphabet[i]){
-             dup = true;
-           }
-         }
-         if(dup == false){
-           alphabet[count] = c;
-           count++;
-         }
-         
-       }
+      for(char c : fileArray){
+      //System.out.println(c);
+        dup = false;
+        for(int i = 0; i < alphabet.length; i++){
+          if(c == alphabet[i]){
+            dup = true;
+          }
+        }
+        if(dup == false){
+          alphabet[count] = c;
+          count++;
+        }
+      }
        
-       //for(char c : alphabet){
-         System.out.println(alphabet);
-       //}
-       
+      System.out.println("Alphabet of file is:");
+      System.out.println(alphabet);
+
       
       
+      Scanner input2 = new Scanner(System.in);
       
-      /*
       // Scans through each line of an input file
-      while (input.hasNextLine()){
+      while (input2.hasNextLine()){
         // String to be scanned to find the pattern.
-        String line = input.nextLine();
+        String line = input2.nextLine();
+        System.out.println(line);
         
         // regular expression 
         // this one finds strings in alphabet {a,b} where number of a's is odd
@@ -77,22 +85,12 @@
         } else {
           System.out.println("NO MATCH");
         }
-      }*/
-   }
-   /*
-   public static void learnAlphabet(Scanner input){
-     String alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-     int[] count = new int[52];
-     char[] alphabet = char[5];
-     
-     while(input.hasNext()){
-       String file += input.Next();
+      }
      }
      
-     char[] fileArray = file.toCharArray();
-     
-     
-     
-     
-   }*/
+     catch (FileNotFoundException ex){
+       System.out.println("ERROr");
+     }
+   }
+
  }
